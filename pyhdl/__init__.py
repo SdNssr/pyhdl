5fd3e0ee4c9ab1e4b1077f46e6560da922ca957d
SdNssr <SdNssr@users.noreply.github.com>
2016-04-20 14:00:36 +0530

    Added __all__ to __init__.py to simplify imports.

---
+++ pyhdl/__init__.py
@@ -0,0 +1,16 @@
+from pyhdl.wire import *
+from pyhdl.primitives import *
+from pyhdl.declarative import Module
+
+__all__ = [
+    'Wire', 
+    'SubWire', 
+    'Module',
+    'NandGate',
+    'AndGate',
+    'NorGate',
+    'OrGate',
+    'XorGate',
+    'NotGate',
+    'DFF'
+]
