31d8b5f210ffa8b658a05dced2a9038e1c76cc86
SdNssr <SdNssr@users.noreply.github.com>
2016-04-20 13:19:24 +0530

    Fixed setup.py (Again!)

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+"""
+PyHDL
+-----
+A simple HDL written in Python.
+"""
+
 import os
 from setuptools import setup
 
@@ -8,10 +14,14 @@ setup(
     name = "pyhdl",
     version = "0.1.0",
     description = ("A simpe HDL for learning hardware design."),
-    long_description=read('README.md'),
+    long_description=__doc__,
     classifiers=[
         "Development Status :: 3 - Alpha",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
